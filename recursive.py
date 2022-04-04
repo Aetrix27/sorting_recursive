c6e38a5a3586e97910ad30c3fbf28f2f20cb2ed8
David <davedogg323@gmail.com>
2022-04-04 13:53:45 -0700

    Completed merge sort

--- recursive.py
+++ recursive.py
@@ -9,7 +9,7 @@ def merge(items1, items2):
     and return a new list containing all items in sorted order.
     Running time: O(1) average, since it traverses through both at the same time
     Memory usage: O(1), no extra memory being used """
-     # Repeat until one list is empty
+    # Repeat until one list is empty
     #  Find minimum item in both lists and append it to new list
     # Append remaining items in non-empty list to new list
 
@@ -31,22 +31,51 @@ def merge(items1, items2):
     return merged_list
         
    
-print(merge([1,2,3],[2,4,6]))
+print(merge([1,2,6],[2,4,3]))
 
 def merge_sort(items):
     """Sort given items by splitting list into two approximately equal halves,
     sorting each recursively, and merging results into a list in sorted order.
-    TODO: Running time: ??? Why and under what conditions?
-    TODO: Memory usage: ??? Why and under what conditions?"""
-    partition = len(items) // 2
-    first = items[:partition]
-    second = items[partition:]
+    Running time: O(n * logn) for every case, since it always divides the array and compares each time.
+    Memory usage: O(n) in all cases since it must copy the data when sorting."""
+    #Check if list is so small it's already sorted (base case)
+    if len(items) == 1:
+        return items
+    elif len(items) > 1:
+        #Split items list into approximately equal halves
+        mid = len(items)//2
+        first_part = items[:mid]
+        second_part = items[mid:]
+        #Sort each half by recursively calling merge sort
+        merge_sort(first_part)
+        merge_sort(second_part)
+  
+        i = 0
+        j = 0
+        k = 0
+  
+        while i < len(first_part) and j < len(second_part):
+            if first_part[i] < second_part[j]:
+                items[k] = first_part[i]
+                i += 1
+            else:
+                items[k] = second_part[j]
+                j += 1
+            k += 1
 
-    # TODO: Check if list is so small it's already sorted (base case)
-    # TODO: Split items list into approximately equal halves
-    # TODO: Sort each half by recursively calling merge sort
-    # TODO: Merge sorted halves into one list in sorted order
-
+        #Merge sorted halves into one list in sorted order
+        while i < len(first_part):
+            items[k] = first_part[i]
+            i += 1
+            k += 1
+  
+        while j < len(second_part):
+            items[k] = second_part[j]
+            j += 1
+            k += 1
+    return items
+  
+print(merge_sort([1,7,3,6,2]))
 
 def partition(items, low, high):
     """Return index `p` after in-place partitioning given items in range
